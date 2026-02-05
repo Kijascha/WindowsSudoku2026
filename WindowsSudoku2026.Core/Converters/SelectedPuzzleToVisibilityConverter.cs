da2acde926b023c30f038884d4d8745a9309d920
Kijascha <keyara@gmx.de>
2026-02-05 16:39:35 +0100

    The delete button should only be visible on the selected puzzle item

---
+++ WindowsSudoku2026.Core/Converters/SelectedPuzzleToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using WindowsSudoku2026.Common.DTO;
+
+namespace WindowsSudoku2026.Core.Converters
+{
+    public class SelectedPuzzleToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is PuzzleDTO selectedPuzzle && values[1] is PuzzleDTO currentPuzzle)
+            {
+                if (selectedPuzzle.Id != currentPuzzle.Id) return Visibility.Hidden;
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+         => [Binding.DoNothing, Binding.DoNothing];
+    }
+}
